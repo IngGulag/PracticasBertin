ac2e672305c9457ace12635e0d29095bcadb1994
trejoang <trejoang>
2025-02-25 15:13:35 -0600

    hola desde mi primer repositorio

---
+++ src/com/mx/Libro/IMetodos.java
@@ -0,0 +1,11 @@
+package com.mx.Libro;
+
+public interface IMetodos {
+	//void: es un tipo de valor de retorno
+	public void guardar(Libro libro);
+	public void editar(int indice, Libro libro);
+	public void mostar();
+	public void eliminar(int indice);
+	public Libro buscar(int indice);
+
+}
